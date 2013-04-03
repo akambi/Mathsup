e8d6b32b6baead8336fbedebab494ffe2aeadf69
akambi <contact@akambi-fagbohoun.com>
2013-04-03 11:31:40 +0100

    TABLE DEPARTEMENT

---
+++ src/Msp/FrontendBundle/Entity/Departement.php
@@ -0,0 +1,109 @@
+<?php
+
+namespace Msp\FrontendBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Departement
+ *
+ * @ORM\Table()
+ * @ORM\Entity(repositoryClass="Msp\FrontendBundle\Entity\DepartementRepository")
+ */
+class Departement
+{
+    /**
+    * @ORM\OneToMany(targetEntity="Msp\UserBundle\Entity\User", mappedBy="departement")
+    */
+    private $users;
+    
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="libelle", type="string", length=100)
+     */
+    private $libelle;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set libelle
+     *
+     * @param string $libelle
+     * @return Departement
+     */
+    public function setLibelle($libelle)
+    {
+        $this->libelle = $libelle;
+    
+        return $this;
+    }
+
+    /**
+     * Get libelle
+     *
+     * @return string 
+     */
+    public function getLibelle()
+    {
+        return $this->libelle;
+    }
+    /**
+     * Constructor
+     */
+    public function __construct()
+    {
+        $this->users = new \Doctrine\Common\Collections\ArrayCollection();
+    }
+    
+    /**
+     * Add users
+     *
+     * @param \Msp\UserBundle\Entity\User $users
+     * @return Departement
+     */
+    public function addUser(\Msp\UserBundle\Entity\User $users)
+    {
+        $this->users[] = $users;
+    
+        return $this;
+    }
+
+    /**
+     * Remove users
+     *
+     * @param \Msp\UserBundle\Entity\User $users
+     */
+    public function removeUser(\Msp\UserBundle\Entity\User $users)
+    {
+        $this->users->removeElement($users);
+    }
+
+    /**
+     * Get users
+     *
+     * @return \Doctrine\Common\Collections\Collection 
+     */
+    public function getUsers()
+    {
+        return $this->users;
+    }
+}
